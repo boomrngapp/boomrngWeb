2577ef6350f60a777d2bf18c07c80b9450c961de
Owen Buckley <obuckleyjr@gmail.com>
2021-02-06 07:21:22 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Boomrng .
+        </p>
+        <a
+          className="App-link"
+          href="https://landing.boomrng.us"
+          target="_blank"
+          rel="noopener noreferrer"
+        >Connect with us!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
